b1ccc00881f79f1ff71a6ceb2b75aee30092e1b4
whddbsl <whddbs04140@naver.com>
2023-07-13 02:51:12 +0900

    checkEmail, checkPw 함수 추가

--- mission01/naver_login/js/main.js
+++ mission01/naver_login/js/main.js
@@ -1,8 +1,7 @@
-
 const user = {
-  id:'asd@naver.com',
-  pw:'spdlqj123!@'
-}
+  id: "asd@naver.com",
+  pw: "spdlqj123!@",
+};
 
 /*
 
@@ -12,66 +11,85 @@ const user = {
 4. 로그인 버튼을 클릭시 조건처리
 
 */
-const email = document.getElementById('userEmail');
-const pw = document.getElementById('userPassword');
-const loginButton = document.querySelector('button');
+const email = document.getElementById("userEmail");
+const pw = document.getElementById("userPassword");
+const loginButton = document.querySelector("button");
 
-let inputEmail = '';
-let inputPw = '';
+let inputEmail = "";
+let inputPw = "";
 let emailValid = false;
 let pwValid = false;
 
-function getLoginInput(){
+function getLoginInput() {
   inputEmail = email.value;
   inputPw = pw.value;
 }
 
-function checkValid(){
-  getLoginInput();
-
+function checkEmail() {
   emailValid = emailReg(inputEmail);
-  pwValid = pwReg(inputPw);
-  
-  if(inputEmail === user.id && inputPw === user.pw){
-    window.location.href = 'welcome.html'
+
+  if (!emailValid) {
+    email.classList.add("is--invalid");
+  } else {
+    email.classList.remove("is--invalid");
   }
 
-  if(!emailValid){
-    email.classList.add('is--invalid')
-  }else{
-    email.classList.remove('is--invalid')
+  if (inputEmail !== user.id) {
+    alert("아이디가 잘못 되었습니다");
   }
-
-  if(!pwValid){
-    pw.classList.add('is--invalid')
-  }else{
-    pw.classList.remove('is--invalid')
-  }
-  event.preventDefault()
 }
 
-loginButton.addEventListener("click", checkValid)
+// function checkPw() {
+//   pwValid = pwReg(inputPw);
 
+//   if (!pwValid) {
+//     pw.classList.add("is--invalid");
+//   } else {
+//     pw.classList.remove("is--invalid");
+//   }
+
+//   if (inputPw !== user.pw) {
+//     alert("비밀번호가 잘못 되었습니다.");
+//   }
+// }
+
+const checkPw = () => {
+  const pwValid = pwReg(inputPw);
+
+  if (!pwValid) {
+    pw.classList.add("is--invalid");
+  } else {
+    pw.classList.remove("is--invalid");
+  }
+
+  if (inputPw !== user.pw) {
+    alert("비밀번호가 잘못 되었습니다.");
+  }
+};
+
+function checkValid() {
+  getLoginInput();
+  checkEmail();
+  checkPw();
+
+  if (inputEmail === user.id && inputPw === user.pw) {
+    window.location.href = "welcome.html";
+  }
+
+  event.preventDefault();
+}
+
+loginButton.addEventListener("click", checkValid);
 
 // 정규식
-function emailReg(text){
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+function emailReg(text) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  return re.test(String(text).toLowerCase())
-}
-
-function pwReg(text){
-  const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
   return re.test(String(text).toLowerCase());
 }
 
-
-
-
-
-
-
-
-
-
-
+function pwReg(text) {
+  const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
+  return re.test(String(text).toLowerCase());
+}
