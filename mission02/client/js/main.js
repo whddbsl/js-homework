2b8cb84578cae3f5b30ad448b405ab64d018e0ea
whddbsl <whddbs04140@naver.com>
2023-07-22 13:37:52 +0900

    audio설정 완료

--- mission02/client/js/main.js
+++ mission02/client/js/main.js
@@ -8,33 +8,52 @@
 
 */
 
-const nav = getNode(".nav");
-const visualImage = getNode(".visual img");
-const nickName = getNode('.nickName');
+function changeNickname(index) {
+  const nickName = getNode(".nickName");
+  nickName.textContent = data[index - 1].name;
+}
 
-nav.addEventListener("click", handleSlider);
+function changeImage(index) {
+  const visualImage = getNode(".visual img");
+  attr(
+    visualImage,
+    "src",
+    `./assets/${data[index - 1].name.toLowerCase()}.jpeg`
+  );
+  attr(visualImage, "alt", data[index - 1].alt);
+}
 
-function handleSlider(e){
-    e.preventDefault();
-    
-    const target = e.target.closest('li');
-    
-    if(!target) return;
-    
-    const li = getNodes('li')
-    const index = attr(target,'data-index');
-    
-    li.forEach(li => removeClass(li,'is-active'));
-  
-    addClass(target,'is-active')
-    
-    nickName.textContent = data[index-1].name
-    
-    attr(visualImage,'src',`./assets/${data[index-1].name.toLowerCase()}.jpeg`)
-    attr(visualImage,'alt',data[index -1].alt)
+function changeBGC(index) {
+  const body = getNode("body");
+  const colorA = data[index - 1].color[0];
+  const colorB = data[index - 1].color[1];
+  body.style.background = `linear-gradient(to bottom, ${colorA}, ${colorB})`;
+}
 
-    const colorA = data[index-1].color[0]
-    const colorB = data[index-1].color[1]
-    console.log(colorA);
-    document.body.style.background = `linear-gradient(to bottom, ${colorA}, ${colorB})`;
-  }
+function createAudio(index) {
+  const audioSource = `assets/audio/${data[index - 1].name.toLowerCase()}.m4a`;
+  const audio = new AudioPlayer(audioSource);
+  audio.play();
+}
+
+function handlePoster(e) {
+  const target = e.target.closest("li");
+
+  if (!target) return;
+
+  const li = getNodes("li");
+  const index = attr(target, "data-index");
+  li.forEach((li) => removeClass(li, "is-active"));
+
+  addClass(target, "is-active");
+
+  changeImage(index);
+  changeNickname(index);
+  changeBGC(index);
+  createAudio(index);
+}
+
+(function setEvent() {
+  const nav = getNode(".nav");
+  nav.addEventListener("click", handlePoster);
+})();
